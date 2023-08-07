11d21f71be81995ffc08b37d841aa946e2a2e1b9
VinhTieng <vinhtieng123@gmail.com>
2023-08-07 20:45:21 +0700

    fix logout and add slider

--- Tour/client/src/index.js
+++ Tour/client/src/index.js
@@ -4,8 +4,10 @@ import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
